1f2d734ca230057889d0052ff49ac6925ec036a7
Pasquale Pio Francavilla <pasfranc@gmail.com>
2016-12-14 08:28:46 +0100

    Update city.service.ts
    Removed unused methods

--- app/shared/city.service.ts
+++ app/shared/city.service.ts
@@ -14,18 +14,4 @@ export class CityService {
     getCities(): Promise<City[]> {
         return Promise.resolve(CITIES);
     }
-    getCitiesSlowly(): Promise<City[]> {
-        return new Promise<City[]>(resolve =>
-            setTimeout(resolve, 2000)) // delay 2 seconds
-            .then(() => this.getCities());
-    }
-    getCity(name: string): Promise<City> {
-        const url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+name+'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-        this.http.get(url)
-            .toPromise()
-            .then(response => console.log(response));
-
-        return this.getCities()
-                    .then(cities => cities.find(city => city.name === name));
-    }
 }
